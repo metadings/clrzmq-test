01afcb53ca89d2785dc396023678c800c4151734
metadings <metadings@live.de>
2015-01-08 16:57:31 +0100

    has may be unsupported

---
+++ Program.Has.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Security.Cryptography;
+
+using ZeroMQ;
+
+namespace ZeroMQ.Test
+{
+    static partial class Program
+    {
+        public static void Has(IDictionary<string, string> dict, string[] args)
+        {
+            foreach (string arg in args)
+                Console.WriteLine("{0}: {1}", arg, ZContext.Has(arg));
+        }
+    }
+}
