43b708befd2c57e18bd4d9f23467bae142f298df
metadings <metadings@github.com>
2015-01-08 18:18:28 +0100

    Interesting zmq.Version

--- Program.cs
+++ Program.cs
@@ -14,5 +14,16 @@ namespace ZeroMQ.Test
 
 		public static string Frontend = "tcp://127.0.0.1:2772";
 		public static string Backend = "tcp://127.0.0.1:2773";
+
+		public static void Has(IDictionary<string, string> dict, string[] args)
+		{
+			foreach (string arg in args)
+				Console.WriteLine("{0}: {1}", arg, ZContext.Has(arg));
+		}
+
+		public static void Version(IDictionary<string, string> dict, string[] args)
+		{
+			Console.WriteLine(ZeroMQ.lib.zmq.Version);
+		}
 	}
 }
